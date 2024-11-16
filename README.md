e49344211a0ec5af87932a58b4bd93988153eff6
JesseCrans <jesse.crans@live.nl>
2024-11-16 23:19:30 +0100

    Stripped the preinstalled page to the barebones to make it ready for the project

--- README.md
+++
@@ -1,2 +0,0 @@
-# smite-toernooi
-Website for hosting and keeping track of stats from the 2024 Boys Smite Tournament.
---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Smite Toernooi",
+  description: "De Boys Smite Toernooi 2024",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
