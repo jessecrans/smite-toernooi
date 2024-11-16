471f3da1d25ed24723d7d339938f3239aa897c4e
JesseCrans <jesse.crans@live.nl>
2024-11-16 23:53:31 +0100

    added some basic info

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="h-screen flex justify-center items-center">
         {children}
       </body>
     </html>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
+    <main className="flex flex-col justify-between items-center">
+      <h2 className="text-4xl">De Boys</h2>
+      <h1 className="text-8xl uppercase">Smite Toernooi</h1>
+      <h2 className="text-4xl">2024</h2>
+      <Image
+        src="/pip.jpg"
+        alt="Pip"
+        width={500}
+        height={500}
+        className=""
+      />
+      {/* <button className="bg-blue-500 hover:bg-blue-700 font-bold p-4 m-4 rounded text-white">
+        Coming soon...
+      </button> */}
     </main>
   );
 }
